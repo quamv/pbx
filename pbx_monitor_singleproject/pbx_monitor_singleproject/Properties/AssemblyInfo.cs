7a48f4f7d5e0883761c2c34d1c0a4522d6f02e3c
QuaMV <quamv@outlook.com>
2016-11-26 16:56:30 -0500

    capturing the single project implementation. it wasn't committable so we will grab it here and then remove from the project.

---
+++ pbx_monitor_singleproject/pbx_monitor_singleproject/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("pbx_monitor_singleproject")]
+[assembly: AssemblyDescription("console based pbx event queue monitor with no project dependencies, only dlls")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pbx_monitor_singleproject")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dcc6c69a-0b2b-46f7-8c90-154a030a28da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ pbx_monitor_singleproject/pbx_monitor_singleproject/pbx_monitor_console.cs
@@ -0,0 +1,110 @@
+﻿using msmq_base_support;
+using pbx_dto_lib;
+using System;
+using System.Collections.Generic;
+
+namespace pbx_monitor_singleproject
+{
+    class pbx_monitor_console
+    {
+        static void Main(string[] args)
+        {
+            var queue_name = "phoneswitchevents";// Properties.Settings.Default.queue_name;
+
+            Console.WriteLine("MSMQ Queue Name: " + queue_name);
+
+            iBlockingReadonlyQueue<pbx_dto> q = new blocking_msmq_queue_of_t<pbx_dto>(
+                            queue_name,
+                            CustomMessageClass._supportedtypes,
+                            CustomMessageClass._parse_msmq_message);
+
+
+            while (true)
+            {
+                var dto = q.Take();
+
+                Console.WriteLine(dto._dto_type.ToString() + " Event:");
+
+                event_handlers[dto._dto_type](dto);
+            }
+        }
+
+
+        /*
+         * event-specific handlers 
+         */
+        private class event_handler_dict : Dictionary<pbx_dto.dto_type, Action<pbx_dto>> { }
+        private static event_handler_dict event_handlers = new event_handler_dict()
+        {
+            {
+                pbx_dto.dto_type.callanswered,
+                (dto) => { Console.WriteLine("Call ID: " + ((pbx_dto_callanswered)dto).callid); }
+            },
+            {
+                pbx_dto.dto_type.callended,
+                (dto) => {
+                    var dto2 = (pbx_dto_callended)dto;
+                    Console.WriteLine("Call ID: " + dto2.callid);
+                    Console.WriteLine("Event Date: " + dto2.createdate);
+                }
+            },
+            {
+                pbx_dto.dto_type.callreceived,
+                (dto) => {
+                    var dto2 = (pbx_dto_callreceived)dto;
+                    Console.WriteLine("Call Details:");
+                    Console.WriteLine("Event Date: " + dto2.createdate);
+                    Console.WriteLine("Direction: " + dto2.call_dto.direction);
+                    Console.WriteLine("Local Number: " + dto2.call_dto.localnbr);
+                    Console.WriteLine("Dialed #: " + dto2.call_dto.dialednbr);
+                }
+            },
+            {
+                pbx_dto.dto_type.callstatechanged,
+                (dto) => {
+                    var dto2 = (pbx_dto_callstatechanged)dto;
+                    Console.WriteLine("Call ID: " + dto2.callid);
+                    Console.WriteLine("Event Date: " + dto2.createdate);
+                    Console.WriteLine("New State: " + dto2.newstate);
+                }
+            },
+            {
+                pbx_dto.dto_type.calltransferred,
+                (dto) => {
+                    var dto2 = (pbx_dto_calltransferred)dto;
+                    Console.WriteLine("Call ID: " + dto2.callid);
+                    Console.WriteLine("Event Date: " + dto2.createdate);
+                    Console.WriteLine("From Ext #: " + dto2.from_extension);
+                    Console.WriteLine("To Ext #: " + dto2.to_extension);
+                }
+            },
+            {
+                pbx_dto.dto_type.secondary_extension_added,
+                (dto) => {
+                    var dto2 = (pbx_dto_secondary_extension_added)dto;
+                    Console.WriteLine("Call ID: " + dto2.callid);
+                    Console.WriteLine("Event Date: " + dto2.createdate);
+                    Console.WriteLine("Ext #: " + dto2.extension_nbr);
+                }
+            },
+            {
+                pbx_dto.dto_type.secondary_extension_removed,
+                (dto) => {
+                    var dto2 = (pbx_dto_secondary_extension_removed)dto;
+                    Console.WriteLine("Call ID: " + dto2.callid);
+                    Console.WriteLine("Event Date: " + dto2.createdate);
+                    Console.WriteLine("Ext #: " + dto2.extension_nbr);
+                }
+            },
+            {
+                pbx_dto.dto_type.unknown,
+                (dto) => {
+                    Console.WriteLine("Unknown event");
+                    Console.WriteLine("Event Date: " + dto.createdate);
+                }
+            }
+
+        };
+
+    }
+}
